78e61afee288e0f0da62d8568922f2d564e6693f
郵差 <a34007024@gmail.com>
2021-04-22 20:38:32 +0800

    try with website URL

--- 1092DataWarehouse_WebCrawler_Tutorial/Week7/ex1/ch5_8.py
+++ 1092DataWarehouse_WebCrawler_Tutorial/Week7/ex1/ch5_8.py
@@ -1,19 +1,12 @@
 # ch5_8.py
 import bs4
+import requests
 
-htmlFile = open('myhtml.html', encoding='utf-8')
-objSoup = bs4.BeautifulSoup(htmlFile, 'lxml')
-objTag = objSoup.select('#author')
+htmlFile = requests.get('https://ithelp.ithome.com.tw/articles/10186119')
+# print(htmlFile.text)
+objSoup = bs4.BeautifulSoup(htmlFile.text, 'lxml')
+objTag = objSoup.select('#fb-root') # element id
 print("資料型態     = ", type(objTag))          # 列印資料型態
 print("串列長度     = ", len(objTag))           # 列印串列長度
 print("元素資料型態 = ", type(objTag[0]))       # 列印元素資料型態
 print("元素內容     = ", objTag[0].getText())   # 列印元素內容
-
-
-
-
-
-
-
-
-
