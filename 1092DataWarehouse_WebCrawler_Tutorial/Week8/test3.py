d5f202e351773dedf5a3ea58e3bebee4e160a6a0
郵差 <a34007024@gmail.com>
2021-04-22 21:11:09 +0800

    add chromedriver path

--- 1092DataWarehouse_WebCrawler_Tutorial/Week8/test3.py
+++ 1092DataWarehouse_WebCrawler_Tutorial/Week8/test3.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome()
+browser = webdriver.Chrome("C:\\[Git_Repos]\\Python_Playground\\1092DataWarehouse_WebCrawler_Tutorial\\Week8\\chromedriver.exe")
 browser.get('https://aaa.24ht.com.tw')                # 網頁下載至瀏覽器
 print('瀏覽器名稱 = ', browser.name)             # 列印瀏覽器名稱
 print('網頁url    = ', browser.current_url)      # 列印網頁url
