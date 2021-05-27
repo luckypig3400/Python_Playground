a7b64e6fb24794de6173e40a4ad29e1145d075e4
郵差 <a34007024@gmail.com>
2021-05-27 20:41:21 +0800

    prepare to crawl climate data for Final Report

---
+++ 1092DataWarehouse_FinalReport/taipeiCrawl.py
@@ -0,0 +1,11 @@
+import datetime
+import requests
+from bs4 import BeautifulSoup
+
+# html = requests.get("https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station=466920&stname=%25E8%2587%25BA%25E5%258C%2597&datepicker=2021-05-20#")
+# print(html.text)
+
+x = datetime.datetime(2020, 5, 17)
+print(x)
+
+# soup = BeautifulSoup(html.text, "html.parser")
