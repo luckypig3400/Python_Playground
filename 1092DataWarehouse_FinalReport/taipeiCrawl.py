7c8857cb3f9b0ed57c7d02eecd9cde0e8d7ec6f7
郵差 <a34007024@gmail.com>
2021-05-27 21:48:31 +0800

    import chromedriver and get url

--- 1092DataWarehouse_FinalReport/taipeiCrawl.py
+++ 1092DataWarehouse_FinalReport/taipeiCrawl.py
@@ -1,10 +1,15 @@
 import datetime
+from selenium import webdriver
+
+driver = webdriver.Chrome('C:\\[Git_Repos]\\Python_Playground\\chromeDriver-win32\\chromedriver.exe')
 
 startDate = datetime.datetime(2020, 1, 1)
 # https://www.w3schools.com/python/python_datetime.asp
 
 for i in range(365):
-    print(startDate)
+    # print(startDate)
     startDate += datetime.timedelta(days=1)
     # https://stackoverflow.com/questions/3240458/how-to-increment-a-datetime-by-one-day
 
+url = 'https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station=466920&stname=%25E8%2587%25BA%25E5%258C%2597&datepicker=2021-05-20#'
+driver.get(url)
