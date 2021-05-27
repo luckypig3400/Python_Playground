2b96c418c165bd3feed012d0ea2f0a72ca8040af
郵差 <a34007024@gmail.com>
2021-05-27 21:05:54 +0800

    increase time day by day

--- 1092DataWarehouse_FinalReport/taipeiCrawl.py
+++ 1092DataWarehouse_FinalReport/taipeiCrawl.py
@@ -2,10 +2,15 @@ import datetime
 import requests
 from bs4 import BeautifulSoup
 
+startDate = datetime.datetime(2020, 1, 1)
+# https://www.w3schools.com/python/python_datetime.asp
+
+for i in range(365):
+    print(startDate)
+    startDate += datetime.timedelta(days=1)
+    # https://stackoverflow.com/questions/3240458/how-to-increment-a-datetime-by-one-day
+
 # html = requests.get("https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station=466920&stname=%25E8%2587%25BA%25E5%258C%2597&datepicker=2021-05-20#")
 # print(html.text)
-
-x = datetime.datetime(2020, 5, 17)
-print(x)
 
 # soup = BeautifulSoup(html.text, "html.parser")
