210766b8cfa19bf66459f0f9c0c78755e0bfb760
郵差 <a34007024@gmail.com>
2021-05-28 12:10:32 +0800

    crawl station 466920 1 year data in loop

--- 1092DataWarehouse_FinalReport/taipeiCrawl.py
+++ 1092DataWarehouse_FinalReport/taipeiCrawl.py
@@ -18,12 +18,14 @@ for i in range(365):
     # print(startDate)
     startDate += datetime.timedelta(days=1)
     # https://stackoverflow.com/questions/3240458/how-to-increment-a-datetime-by-one-day
+    dateString = str(startDate.date())
+    print('Current crawling:' + dateString)
 
-url = 'https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station=466920&stname=%25E8%2587%25BA%25E5%258C%2597&datepicker=2021-05-20#'
-driver.get(url)
+    url = 'https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station=466920&stname=%25E8%2587%25BA%25E5%258C%2597&datepicker=' + dateString
+    driver.get(url)
 
-csvFile = driver.find_element_by_id('downloadCSV')
-csvFile.click()
-time.sleep(1)
+    csvFile = driver.find_element_by_id('downloadCSV')
+    csvFile.click()
+    time.sleep(1)
 
 driver.close()
