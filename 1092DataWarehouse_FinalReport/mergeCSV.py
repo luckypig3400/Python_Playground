2b167a16d43f5a0282197eaddc66c7a5bfdfeca8
郵差 <a34007024@gmail.com>
2021-06-01 23:15:47 +0800

    fix small bugs (head line and readline sequence)

--- 1092DataWarehouse_FinalReport/mergeCSV.py
+++ 1092DataWarehouse_FinalReport/mergeCSV.py
@@ -25,7 +25,7 @@ mergedFile = codecs.open(mergedFileName, "a+", "utf-8")
 # https://stackoverflow.com/questions/934160/write-to-utf-8-file-in-python
 
 mergedFile.write(u"\"測站名稱\",\"觀測日期\",\"觀測時間(hour)\",\"測站氣壓(hPa)\",\"海平面氣壓(hPa)\",\"氣溫(℃)\",\"露點溫度(℃)\",\"相對溼度(%)\",\"風速(m/s)\",\"風向(360degree)\",\"最大陣風(m/s)\",\"最大陣風風向(360degree)\",\"降水量(mm)\",\"降水時數(hr)\",\"日照時數(hr)\",\"全天空日射量(MJ/㎡)\",\"能見度(km)\",\"紫外線指數\",\"總雲量(0~10)\"\n")
-mergedFile.write(u"\"StnName\",\"ObsDate\",\"ObsTime\",\"StnPres\",\"SeaPres\",\"Temperature\",\"Td dew point\",\"RH\",\"WS\",\"WD\",\"WSGust\",\"WDGust\",\"Precp\",\"PrecpHour\",\"SunShine\",\"GloblRad\",\"Visb\",\"UVI\",\"Cloud Amount\"\n")
+mergedFile.write(u"\"StnName\",\"ObsDate\",\"ObsTime\",\"StnPres\",\"SeaPres\",\"Temperature\",\"Td dew point\",\"RH\",\"WS\",\"WD\",\"WSGust\",\"WDGust\",\"Precp\",\"PrecpHour\",\"SunShine\",\"GloblRad\",\"Visb\",\"UVI\",\"Cloud Amount\"")
 
 for fileName in onlyfiles:
     print(fileName)
@@ -43,12 +43,13 @@ for fileName in onlyfiles:
         unmergeFile.readline()
         # 前兩列為標題 直接儀器不使用
 
-        line = "start to read~"
-        while len(line) != 0:
-            line = unmergeFile.readline()
+        mergedFile.write("\n")  # 在寫入每個檔案前換行
+        line = unmergeFile.readline()
+        while len(line) != 0:  # https://www.coder4.com/archives/3101
             mergedFile.write("\"" + currentDirName + "\",\"")
             mergedFile.write(date + "\",")
             mergedFile.write(line)
+            line = unmergeFile.readline()
 
         unmergeFile.close()
 
