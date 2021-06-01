4cd8e307934b6ee6b6680e8fc419f4a142a66b03
郵差 <a34007024@gmail.com>
2021-06-01 17:25:40 +0800

    get parent directory path and subtract path strings

--- 1092DataWarehouse_FinalReport/mergeCSV.py
+++ 1092DataWarehouse_FinalReport/mergeCSV.py
@@ -1,11 +1,14 @@
-from os import cpu_count, listdir, path
-from os.path import isfile, join
-import pathlib
+import os
 
-currentDirName = pathlib.Path()
-currentDirPath = pathlib.Path.absolute()
-# https://stackoverflow.com/questions/3430372/how-do-i-get-the-full-path-of-the-current-files-directory/3430395
+currentDirPath = os.path.abspath(os.curdir)
+print(currentDirPath)
 
+os.chdir("..")
+# https://stackoverflow.com/questions/17885516/moving-up-one-directory-in-python/41652554
+parentDirPath = os.path.abspath(os.curdir)
+os.chdir(currentDirPath)
+
+currentDirName = currentDirPath - parentDirPath
 print(currentDirName)
 
 # onlyfiles = [f for f in listdir()]
